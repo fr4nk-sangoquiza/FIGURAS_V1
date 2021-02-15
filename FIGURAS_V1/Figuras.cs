95a8f22b18b0b0cef0286e9079a40ff5584771f0
UsuarioFS <UsuarioFS@PC-FRANKLIN>
2021-02-16 00:21:42 +0100

    Agregar archivos de proyecto.

---
+++ FIGURAS_V1/Figuras.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIGURAS_V1
+{
+    class Figuras       // class Figuras:Program  => Estoy diciendo que herede todo de program por ejemplo
+    {
+
+        // PROPIEDADES      ¿Qué tiene una figura?
+
+        public decimal area {get; set;}      // 'get' y 'set' son de tipo lectura y escritura. Sintaxis para declarar un atributo
+        decimal lado;
+        decimal ancho;
+        decimal radio;
+
+        // FUNCIONES
+
+        public void areaCuadrado ()
+        {
+            Console.WriteLine("Ingresa el número: ");
+            lado = Convert.ToDecimal(Console.ReadLine());
+            area = lado * lado;
+
+            Console.WriteLine( "El resultado del area es: " + area);
+        }
+
+
+
+
+
+    }
+}
---
+++ FIGURAS_V1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FIGURAS_V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figuras figura_fs = new Figuras();      // Instanciando una clase
+            figura_fs.areaCuadrado();       //acedemos al método creado
+            Console.ReadKey();
+
+        }
+    }
+}
