2cc436d05f7bbd6b7c68c07b5ee524931b813e36
UsuarioFS <UsuarioFS@PC-FRANKLIN>
2021-02-16 20:42:26 +0100

    Aplicación finalizada

--- FIGURAS_V1/Figuras.cs
+++ FIGURAS_V1/Figuras.cs
@@ -4,16 +4,16 @@ using System.Text;
 
 namespace FIGURAS_V1
 {
-    class Figuras       // class Figuras:Program  => Estoy diciendo que herede todo de program por ejemplo
+    class Figuras       
     {
 
         // PROPIEDADES      ¿Qué tiene una figura?
 
-        //public decimal area {get; set;}      // 'get' y 'set' son de tipo lectura y escritura. Sintaxis para declarar un atributo
         double area=0;
         double altura=0;
         double basee=0;
         double radio=0;
+
 
         // FUNCIONES
 
@@ -26,14 +26,13 @@ namespace FIGURAS_V1
             Console.WriteLine( "El resultado del area es: " + area);
         }
 
-        public void areaRectangulo()        // void no devuelve nada, es decir, no tiene return
+        public void areaRectangulo()       
         {
             Console.WriteLine("Ingresa la altura: ");
             altura = Convert.ToDouble(Console.ReadLine());
             
             Console.WriteLine("Ingresa la base: ");
             basee = Convert.ToDouble(Console.ReadLine());
-
             area = basee * altura;
 
             Console.WriteLine("El resultado del area es: " + area);
@@ -45,7 +44,6 @@ namespace FIGURAS_V1
 
             Console.WriteLine("Ingresa el radio del circulo: ");
             radio = Convert.ToDouble(Console.ReadLine());
-
             area = pi * Math.Pow(radio, 2);
 
             Console.WriteLine("El resultado es: " + area );
--- FIGURAS_V1/Program.cs
+++ FIGURAS_V1/Program.cs
@@ -6,20 +6,76 @@ namespace FIGURAS_V1
     {
         static void Main(string[] args)
         {
-            /*
-            Figuras figura_cuad = new Figuras();      // Instanciando una clase
-            figura_cuad.areaCuadrado();               //acedemos al método creado
-            Console.ReadKey();
-  
-            Figuras figura_rec = new Figuras();
-            figura_rec.areaRectangulo();
-            Console.ReadKey();
-            */
 
-            Figuras figura_cir = new Figuras();
-            figura_cir.areaCirculo();
-            Console.ReadKey();
 
+            Figuras figura_cuad = new Figuras();        
+
+            int contador = 1;
+            try
+            {
+                for (int i = 0; i < contador; i++)
+                {
+                    Console.WriteLine("Desea iniciar la aplicación <S/N>: ");
+                    string valor_elegido=Console.ReadLine();
+
+                    if (valor_elegido.ToUpper() == "S")     
+                    {
+                        Console.WriteLine("Presiona el número de la opción que prefieras: ");
+                        Console.WriteLine(" 1  Área del cuadrado");
+                        Console.WriteLine(" 2  Área del rectángulo");
+                        Console.WriteLine(" 3  Área del circulo");
+
+
+                        int opcion = Convert.ToInt32(Console.ReadLine());
+
+                        switch (opcion)
+                        {
+                            case 1:
+                                figura_cuad.areaCuadrado();
+                                Console.ReadKey();
+                                break;
+                            case 2:
+                                figura_cuad.areaRectangulo();
+                                Console.ReadKey();
+                                break;
+                            case 3:
+                                figura_cuad.areaCirculo();
+                                Console.ReadKey();
+                                break;
+                            default:
+                                Console.WriteLine("La opción elegida no es la correcta. Presione INTRO para a iniciar la aplicación.");
+                                Console.ReadKey();
+                                break;
+
+                        }
+                        contador++;
+                        Console.Clear();
+
+                    }
+                    else if (valor_elegido.ToUpper() == "N")
+                    {
+                        //Console.WriteLine("Presione cualquier para salir de la aplicación");
+                        return;      
+                    }
+                    else
+                    {
+                        Console.WriteLine("La opción elegida no es válida. Presione INTRO para volver a seleccionar");
+                        Console.ReadLine();
+                        contador++;
+                        Console.Clear();
+
+                    }
+
+                }
+                Console.ReadKey();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Debes ingresar caracteres numéricos. Contacte con Soporte. " + ex.Message);
+                Console.ReadKey();
+            }
+             
         }
     }
 }
